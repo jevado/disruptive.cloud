3ea9b8463b2f66ad5902949c18c3126428e9ff14
jevado <jeroenvdoorn@gmail.com>
2017-07-02 18:30:12 +0200

    fixing the spec that deliberately failed to see if rspec was decently picket up within cloudbuild

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -4,6 +4,6 @@ describe "sample", type: :feature, js: true do
     # `binding.pry` is useful for crafting the right selector
     # or checking the actual state of the page
     # binding.pry # test will pause here
-    expect(find('.site-title').text).to eq('Your awesome title!!')
+    expect(find('.site-title').text).to eq('Your awesome title')
   end
 end
